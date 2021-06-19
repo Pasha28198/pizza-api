8d99391cf8915f43999c2eed8826290a9f9cb112
pasha28198 <pavlognidin@gmail.com>
2021-06-19 13:07:36 +0300

    add fix

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const whitelist = ['whiteboxpizza.com.ua', 'admin.whiteboxpizza.com.ua'];
 
-  app.enableCors();
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Pizza')
